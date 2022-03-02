5d0459e4afee20dab75d66de71ca97ccdc3304dd
ekdavila <98136237+ekdavila@users.noreply.github.com>
2022-03-02 12:42:24 -0600

    Add files via upload

---
+++ MidtermRCode.R
@@ -0,0 +1,41 @@
+### Using Linear regression
+### Predict the Price (Price) for used Toyota Corolla cars.
+### Use the follwoing three predictors Age_08_04, KM, Fuel_Type
+library(forecast)
+
+car.df <- read.csv("ToyotaCorolla.csv")
+
+
+#Changing to binary
+Fuel_type_num <- ifelse(car.df$Fuel_Type=="Diesel",0,1)
+head(Fuel_type_num)
+
+
+# partition data into Training (70%) and Validation (30%) datasets
+set.seed(1)
+
+train.rows <- sample(rownames(car.df), dim(car.df)[1]*0.7)
+
+train.data<- car.df[train.rows,]
+valid.rows <- setdiff(rownames(car.df), train.rows)
+valid.data<- car.df[valid.rows,]
+# use lm() to run a linear regression of Price on the 
+# three predictors in the training set. 
+model <- lm(Price~ Age_08_04 + KM + Fuel_Type, data = train.data) 
+
+tr.res <- data.frame(train.data$Price, model$fitted.values, model$residuals)
+head(tr.res)
+
+#  use options() to ensure numbers are not displayed in scientific notation.
+options(scipen = 999)
+
+# Display the results (summary()) of the linear regression
+summary(model)
+# use predict() to make predictions on a new set. 
+pred <- predict(model, newdata = valid.data)
+
+
+# use accuracy() to compute common accuracy measures.
+# recall that accuracy is computed against the validation dataset
+
+accuracy(pred, valid.data$Price)
